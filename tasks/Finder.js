051420e45adcfbfbc03ad8abf18e1acdb99e0328
Alexey Chorniy <a.chorniy@digiscorp.com>
2020-03-12 16:58:50 +0200

    Add  Stream

--- tasks/Finder.js
+++ tasks/Finder.js
@@ -1,33 +1,32 @@
-const { promises } = require('fs');
-const { join, relative } = require('path');
-const Searcher = require('./Searcher');
+const { promises } = require("fs");
+const { join, relative } = require("path");
+const Searcher = require("./Searcher");
 
 function startParse(maxDeep, fileName, path, colors, ext, emitter) {
-    const entryPoint = path;
-    return async function finder(path = entryPoint, deep = 0) {
-        const items = await promises.readdir(path, { withFileTypes: true });
-        let files = [];
-        let dirs = [];
-        for (const item of items) {
-            if (item.isFile()) {
-                emitter('found:file');
-                if (Searcher(item.name, path)) {
-                    // console.log('true', item.name);
-                    const relativePath = relative(entryPoint, join(path, item.name))
-                    files = [...files, relativePath];
-                }
-            }
-            else if (item.isDirectory()) {
-                emitter('found:dir');
-                if (!maxDeep || deep < maxDeep) {
-                    dirs = [...dirs, item.name];
-                    const newPath = join(path, item.name);
-                    files = [...files, ...(await finder(newPath, deep + 1))];
-                }
-            }
+  const entryPoint = path;
+  return async function finder(path = entryPoint, deep = 0) {
+    const items = await promises.readdir(path, { withFileTypes: true });
+    let files = [];
+    let dirs = [];
+    for (const item of items) {
+      if (item.isFile()) {
+        emitter("found:file");
+        if (Searcher(item.name, path)) {
+          // console.log('true', item.name);
+          const relativePath = relative(entryPoint, join(path, item.name));
+          files = [...files, relativePath];
         }
-        return files;
-    };
-};
+      } else if (item.isDirectory()) {
+        emitter("found:dir");
+        if (!maxDeep || deep < maxDeep) {
+          dirs = [...dirs, item.name];
+          const newPath = join(path, item.name);
+          files = [...files, ...(await finder(newPath, deep + 1))];
+        }
+      }
+    }
+    return files;
+  };
+}
 
 module.exports = startParse;
--- tasks/Searcher.js
+++ tasks/Searcher.js
@@ -1,35 +1,45 @@
-const { join, relative } = require('path');
-const { createReadStream } = require('fs');
-const FileType = require('file-type');
-const readChunk = require('read-chunk');
-const { extname } = require('path');
-const { fullName } = require('../assets/parseParams');
+const { join, relative } = require("path");
+const { createReadStream } = require("fs");
+const FileType = require("file-type");
+const readChunk = require("read-chunk");
+const { extname } = require("path");
+const { fullName } = require("../assets/parseParams");
 
 function Searcher(itemName, path) {
-    const name = fullName.name;
-    const ext = fullName.ext;
-    const reg = new RegExp(`^${name === '\*' ? '.' : name}${name.includes(' ? ') ? '?' : ''}\\w*\\W*${ext}$`, 'ig');
-    const isMatch = !!itemName.match(reg);
-    if (isMatch) {
-        console.log('FileTypeTrue', name, itemName, ext);
-        // const fullPath = join(path, itemName);
-
-        return true;
+  const name = fullName.name;
+  const ext = fullName.ext;
+  const reg = new RegExp(
+    `^${name === "*" ? "." : name}${
+      name.includes(" ? ") ? "?" : ""
+    }\\w*\\W*${ext}$`,
+    "ig"
+  );
+  const isMatch = !!itemName.match(reg);
+  if (isMatch) {
+    const fullPath = join(path, itemName);
+    const fileExt = extname(itemName);
+    if (fileExt === ".js" || fileExt === ".json") {
+      // const stream = createReadStream(fullPath);
+      // console.log("js", stream);
+      return true;
+    } else {
+      (async () => {
+        try {
+          const stream = createReadStream(fullPath, {
+            highWaterMark: 4100,
+            start: 20,
+            end: 20
+          });
+          const fromStream = await FileType.fromStream(stream);
+          console.log("FileType", fromStream, fileExt, itemName);
+        } catch (error) {
+          console.log("error", error);
+        }
+      })();
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 module.exports = Searcher;
-
-
-// const got = require('got');
-// const FileType = require('file-type');
-
-// const url = 'https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg';
-
-// (async () => {
-//     const stream = got.stream(url);
-
-//     console.log(await FileType.fromStream(stream));
-//     //=> {ext: 'jpg', mime: 'image/jpeg'}
-// })();
