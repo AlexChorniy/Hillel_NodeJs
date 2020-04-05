4456e63b80d475ea2b2dc982355434bd67f7d756
Alexey <alexprudent777@gmail.com>
2020-04-05 22:36:42 +0300

    add project fixes

--- HW1/Hillel-NodeJS-1/1/app.js
+++
@@ -1,6 +0,0 @@
-const { add, add_and_multiply } = require("./add.js");
-const { subtract } = require("./subtract.js");
-const third = require("./multiply");
-
-const result = multiply(3, 2);
-console.log(result);
--- HW1/Hillel-NodeJS-1/1/multiply.js
+++
@@ -1,3 +0,0 @@
-global.multiply = (a, b) => {
-  return a * b;
-};
